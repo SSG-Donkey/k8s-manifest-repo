58b5272d7f814e64fa06e57a2f8ae0f153b5fd22
namjyung <132260170+namjyung@users.noreply.github.com>
2024-05-22 15:00:31 +0900

    Update db-security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -81,3 +81,30 @@ resource "aws_security_group" "private_sg" {
   }
 }
 
+# DB 인스턴스용 보안 그룹 생성
+resource "aws_security_group" "db_sg" {
+    name        = "Project-DB-SG"
+    description = "Security group for Database instances"
+    vpc_id      = aws_vpc.project_vpc.id
+
+    # 인바운드 규칙: DB 접근을 위한 포트 3306을 개방하고, 내부 서브넷에서만 접근 허용
+    ingress {
+        from_port   = 3306
+        to_port     = 3306
+        protocol    = "tcp"
+        security_groups = [aws_security_group.private_sg.id]  # 프라이빗 서브넷 보안 그룹에서만 접근 허용
+    }
+
+    # 아웃바운드 규칙: 모든 트래픽 허용
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    tags = {
+        "Name" = "Project-DB-SG"
+    }
+}
+
