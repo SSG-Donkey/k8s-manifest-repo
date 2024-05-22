4b75c4659cccb3bf8891632a29c0e0059df6f6d7
namjyung <132260170+namjyung@users.noreply.github.com>
2024-05-22 15:07:25 +0900

    Update private-rds.tf

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,13 +1,15 @@
-resource "aws_db_subnet_group" "app_db_subnet_group" {
-  name       = "app-db-subnet-group"
-  subnet_ids = tolist([for s in aws_subnet.app : s.id])
+# DB 서브넷 그룹 생성
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "db-subnet-group"
+  subnet_ids = aws_subnet.db_subnet[*].id 
 
   tags = {
-    Name        = "App DB Subnet Group"
+    Name        = "DB Subnet Group"
     Environment = "production"
   }
 }
 
+# RDS 인스턴스 업데이트
 resource "aws_db_instance" "donkey_rds" {
   identifier             = "donkey-rds"
   allocated_storage      = 20
@@ -16,18 +18,17 @@ resource "aws_db_instance" "donkey_rds" {
   engine_version         = "10.5"
   instance_class         = "db.m6gd.large"
   name                   = "project"
-  username               = "nana"
-  password               = "nana1234"
-  publicly_accessible    = true
+  username               = var.db_user_name
+  password               = var.db_user_pass
+  publicly_accessible    = false
   multi_az               = true
   skip_final_snapshot    = true
 
-  vpc_security_group_ids = [aws_security_group.private_sg.id]
-  db_subnet_group_name   = aws_db_subnet_group.app_db_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group.name
 
   tags = {
     Name = "Donkey-RDS"
     Environment = "production"
   }
 }
-
